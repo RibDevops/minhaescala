6da66cdf5dd81d931121d19ab09288efe5619d60
Ribeiro <ribeiro.ajr@icloud.com>
2026-01-20 15:12:16 -0300

    logs

--- core/settings.py
+++ core/settings.py
@@ -151,3 +151,72 @@ X_FRAME_OPTIONS = 'ALLOWALL'
 LOGIN_URL = 'cal:login'
 LOGIN_REDIRECT_URL = 'cal:home'
 LOGOUT_REDIRECT_URL = 'cal:login'
+
+FORMATTERS = (
+    {
+        "verbose": {
+            "format": "{levelname} {asctime:s} {threadName} {thread:d} {module} {filename} {lineno:d} {name} {funcName} {process:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {asctime:s} {module} {filename} {lineno:d} {funcName} {message}",
+            "style": "{",
+        },
+    },
+)
+
+
+HANDLERS = {
+    "console_handler": {
+        "class": "logging.StreamHandler",
+        "formatter": "simple",
+    },
+    "my_handler": {
+        "class": "logging.handlers.RotatingFileHandler",
+        "filename": f"{BASE_DIR}/logs/blogthedata.log",
+        "mode": "a",
+        "encoding": "utf-8",
+        "formatter": "simple",
+        "backupCount": 5,
+        "maxBytes": 1024 * 1024 * 5,  # 5 MB
+    },
+    "my_handler_detailed": {
+        "class": "logging.handlers.RotatingFileHandler",
+        "filename": f"{BASE_DIR}/logs/django.log",
+        "mode": "a",
+        "formatter": "verbose",
+        "backupCount": 5,
+        "maxBytes": 1024 * 1024 * 5,  # 5 MB
+    },
+}
+
+
+    # DEBUG: Informações de sistema de baixo nível para propósitos de depuração
+    # INFO: Informações gerais de sistema
+    # WARNING: Informações descrevendo a ocorrência de um pequeno problema
+    # ERROR: Informações descrevendo a ocorrência de um problema grande.
+    # CRITICAL: Informações descrevendo a ocorrência de um problema crítico.
+
+LOGGERS = (
+    {
+        "django": {
+            "handlers": ["console_handler", "my_handler_detailed"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["my_handler"],
+            "level": "WARNING",
+            "propagate": False,
+        },
+    },
+)
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": FORMATTERS[0],
+    "handlers": HANDLERS,
+    "loggers": LOGGERS[0],
+}
